d492af3dfed7a5200d11847e0e886596a8b56001
unknown <spencer.jolly@ulb.be>
2025-07-11 14:45:24 +0200

    new stationary phase LC analytical

--- development/RPLB_acc_LC_analytical.py
+++ development/RPLB_acc_LC_analytical.py
@@ -1,47 +1,5 @@
-import cython_wrappers
 import numpy as np
-import scipy.special.cython_special
-from numba.core import cgutils
-from numba.extending import intrinsic,get_cython_function_address
-from numba import jit, njit, types
-import ctypes
-
-##################Intrinsics###################
-@intrinsic
-def val_to_double_ptr(typingctx, data):
-    def impl(context, builder, signature, args):
-        ptr = cgutils.alloca_once_value(builder,args[0])
-        return ptr
-    sig = types.CPointer(types.float64)(types.float64)
-    return sig, impl
-
-@intrinsic
-def double_ptr_to_val(typingctx, data):
-    def impl(context, builder, signature, args):
-        val = builder.load(args[0])
-        return val
-    sig = types.float64(types.CPointer(types.float64))
-    return sig, impl
-
-double = ctypes.c_double
-double_p = ctypes.POINTER(double)
-
-addr = get_cython_function_address("cython_wrappers", "erfc")
-functype = ctypes.CFUNCTYPE(None,double,double,
-                           double_p,double_p,)
-erfc_fn_complex = functype(addr)
-
-@njit("complex128(complex128)")
-def erfc_complex(val):
-    out_real_p=val_to_double_ptr(0.)
-    out_imag_p=val_to_double_ptr(0.)
-
-    erfc_fn_complex(np.real(val), np.imag(val),out_real_p,out_imag_p)
-
-    out_real=double_ptr_to_val(out_real_p)
-    out_imag=double_ptr_to_val(out_imag_p)
-
-    return np.complex(out_real + 1.j * out_imag)
+from numba import jit
 
 @jit(nopython=True)
 def RPLB_acc_LC_analytical(lambda_0, tau_0, w_0, P, Psi_0, phi_2, phi_3, t_0, z_0, beta_0, tau_p):
@@ -52,6 +10,7 @@ def RPLB_acc_LC_analytical(lambda_0, tau_0, w_0, P, Psi_0, phi_2, phi_3, t_0, z_
     e_0 = 8.85418782e-12
     # calculate frequency properties
     omega_0 = 2*np.pi*c/lambda_0
+    delta_omega = 2/tau_0
     # calculate Rayleigh range
     z_R = (omega_0*w_0**2)/(2*c)
     # amplitude factor
@@ -78,17 +37,13 @@ def RPLB_acc_LC_analytical(lambda_0, tau_0, w_0, P, Psi_0, phi_2, phi_3, t_0, z_
     z[0] = beta_0*c*time[0] + z_0*(1-beta_0)
     k_stop = -1
 
-    alpha = tau_0**2 + 2*1j*phi_2
-    b = (tau_p**2)/alpha
-
     #do 5th order Adams-Bashforth finite difference method
     for k in range(0, len(time)-1):
         t_prime = time[k] - z[k]/c
-        a = (1 - 1j*z[k]/z_R) - 2*tau_p*t_prime/alpha
-        const = (tau_0/np.sqrt(alpha))*(2*np.exp(1j*omega_0*t_prime)/z_R)/(8*b**(3/2))
 
-        field_temp = np.exp(1j*Psi_0)*(-1*np.sqrt(np.pi)*a*np.exp(a**2/(4*b))*erfc_complex(a/(2*np.sqrt(b))) + 2*np.sqrt(b))
-        env_temp = np.exp(-(t_prime**2)/alpha)
+        field_temp = np.exp(1j*(omega_0*t_prime - t_prime**2/(2*phi_2) + 2*np.arctan(z_test/z_R - tau_p*t_prime/phi_2)))
+        env_temp = np.exp(-(t_prime/(phi_2*delta_omega))**2)
+        const = np.exp(1j*Psi_0)*(1/(z_R*delta_omega*np.sqrt(np.pi)))*1/(1 + (z[k]/z_R - tau_p*t_prime/phi_2)**2)
         field_total = Amp*field_temp*env_temp*const
         deriv2[k] = (-q_e*np.real(field_total)*((1 - beta[k]**2)**(3/2))/(m_e*c))
 
