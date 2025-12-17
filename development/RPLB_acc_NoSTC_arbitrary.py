f5271292c87b779be98eba07a78944e5f4507533
unknown <spencer.jolly@ulb.be>
2025-12-17 11:03:17 +0100

    update resolution again

--- development/RPLB_acc_NoSTC_arbitrary.py
+++ development/RPLB_acc_NoSTC_arbitrary.py
@@ -20,7 +20,7 @@ def RPLB_acc_NoSTC_arbitrary(lambda_0, tau_0, a, P, PM, phi_2, t_0, z_0, beta_0)
     t_start = t_0/(1-beta_0) + z_0/c
     t_end = +1e5*tau_0
     # number of time steps per laser period
-    n = (lambda_0/(0.8e-6))*np.maximum(50, np.round(np.sqrt(P*tau_0*k_0/(tau*2*a))/(1e10)))
+    n = (lambda_0/(0.8e-6))*np.maximum(50, np.round(np.sqrt(P*tau_0*k_0/(tau*2*a))/(3e10)))
     num_t = np.int_(np.round(n*(t_end-t_start)/(lambda_0/c)))
     time = np.linspace(t_start, t_end, num_t)
     dt = time[1]-time[0]
